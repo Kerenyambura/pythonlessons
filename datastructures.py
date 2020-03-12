ed448e3b04b911f7dde834e83c1b0d3884ebc965
Kerenyambura <kerenwangombe@gmail.com>
2020-03-12 17:48:02 +0300

    Operators and conditional Statements

---
+++ datastructures.py
@@ -0,0 +1,15 @@
+print("1.20MBs")
+print("2.30MBs")
+print("3.40MBs")
+choice=int(input("Enter Selection:   "))
+print(type(choice))
+
+if choice==1:
+    print("You have purchased 20MBs")
+elif choice ==2:
+    print("You have purchased 30MBs")
+elif choice==3:
+    print("You have purchased 40MBS")
+else:
+    print("Invalid Choice")
+
